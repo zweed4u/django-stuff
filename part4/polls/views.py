2d3944b6af7fab75d073461d7e6d9d22a8583eb9
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 19:27:01 -0400

    Before making the views more generic, found missed import from model for Choice object - fixes empty vote - need to include Views in polls.views as changed in polls.urls

--- part4/polls/views.py
+++ part4/polls/views.py
@@ -2,8 +2,9 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 def index(request):
