f01ae7edd521ddd11c9882dfe7be5fe123e1beaa
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 19:21:04 -0400

    Modified polls view so that vote redirects to results and displays updated count - tweaked results html template as well to show this

--- part4/polls/views.py
+++ part4/polls/views.py
@@ -28,8 +28,10 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question {0}".format(question_id)
-	return HttpResponse(response)
+	#response = "You're looking at the results of question {0}".format(question_id)
+	#return HttpResponse(response)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
 	#return HttpResponse("You're voting on question {0}".format(question_id))
