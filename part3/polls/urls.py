6807372b9f3bcdc8392e3b70da059312a02279da
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 15:55:36 -0400

    Added more views - question id, results and vote - and added them to the polls urls

--- part3/polls/urls.py
+++ part3/polls/urls.py
@@ -2,5 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /polls/
 	url(r'^$', views.index, name="index"),
+	
+	# /polls/<id>/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+
+	# /polls/<id>/results/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+
+	# /polls/<id>/vote
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
 ]
--- part3/polls/views.py
+++ part3/polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello World. You're at the polls index")
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at question {0}".format(question_id))
+
+def results(request, question_id):
+	response = "You're looking at the results of question {0}".format(question_id)
+	return HttpResponse(response)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question {0}".format(question_id))
