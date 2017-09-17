be21578918644de85ad5313cf2fae72ee670a5ee
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 15:59:50 -0400

    Modified view to display latest questions - hardcoded

--- part3/polls/views.py
+++ part3/polls/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello World. You're at the polls index")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse("Hello World. You're at the polls index\n{0}".format(output))
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question {0}".format(question_id))
