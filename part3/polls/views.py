7ed7fbd8caf024e6ed711c04ce5fe8f5605e544d
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 16:26:55 -0400

    Using get_object_or_404 shortcut method to rid of try-catch

--- part3/polls/views.py
+++ part3/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
@@ -16,11 +16,14 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+	'''
 	try:
 		question = Question.objects.get(pk=question_id)
 	except Question.DoesNotExist:
 		raise Http404("Question does not exist")
-	#return HttpResponse("You're looking at question {0}".format(question_id))
+	return HttpResponse("You're looking at question {0}".format(question_id))
+	'''
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
