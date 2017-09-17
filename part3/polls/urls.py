3aa8ad00b1e59aa827387f9aabe1eef04ae80b59
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 16:50:55 -0400

    Added specifics word to reflect same as question_id details

--- part3/polls/urls.py
+++ part3/polls/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
 	# /polls/
 	url(r'^$', views.index, name="index"),
 	
-	# /polls/<id>/
+	# /polls/<id>/ - 'name' value as called by url template tag
 	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+
+	#added the word specifics to url
+	url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
 
 	# /polls/<id>/results/
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
