b975f74c1cdde52a1aed194c3f0eda94b0903a51
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 19:23:51 -0400

    Changed url conf (polls.url) to be more generic and use <pk>

--- part4/polls/urls.py
+++ part4/polls/urls.py
@@ -4,17 +4,17 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
 	# /polls/
-	url(r'^$', views.index, name="index"),
+	url(r'^$', views.IndexView.as_view(), name="index"),
 	
 	# /polls/<id>/ - 'name' value as called by url template tag
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 
 	#added the word specifics to url
-	url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+	url(r'^specifics/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 
 	# /polls/<id>/results/
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
 
 	# /polls/<id>/vote
-	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
+	url(r'^(?P<pk>[0-9]+)/vote/$', views.vote, name="vote"),
 ]
