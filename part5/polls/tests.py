dc2d258298becd810a56056f3a2e99f3c2e67fa3
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 19:59:37 -0400

    Fixed typo in Question objects argument in test

--- part5/polls/tests.py
+++ part5/polls/tests.py
@@ -32,6 +32,6 @@ class QuestionModelTests(TestCase):
 		"""
 		# Just falls within our recent threshold by 1 second
 		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-		recent_question = Question(pub_time=time)
+		recent_question = Question(pub_date=time)
 		self.assertIs(recent_question.was_published_recently(), True)
 
