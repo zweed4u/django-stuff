1652f9934878d07bf80c62bda4fea8a938bf2ef9
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 16:07:25 -0400

    Using templates and context to change views - index now shows latest question list

--- part3/polls/views.py
+++ part3/polls/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 # Create your views here.
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse("Hello World. You're at the polls index\n{0}".format(output))
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse("Hello World. You're at the polls index\n{0}".format(template.render(context, request)))
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question {0}".format(question_id))
