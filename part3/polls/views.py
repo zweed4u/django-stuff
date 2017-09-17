71876f54ef41daa980e830a177f912d910b2b599
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 16:14:33 -0400

    Using render to take care of template - returns the HttpResponse object for a tamplate and context - also implemented 404 for detail view

--- part3/polls/views.py
+++ part3/polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -7,14 +8,20 @@ from .models import Question
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	#output = ', '.join([q.question_text for q in latest_question_list])
-	template = loader.get_template('polls/index.html')
+	#template = loader.get_template('polls/index.html')
 	context = {
 		'latest_question_list': latest_question_list,
 	}
-	return HttpResponse("Hello World. You're at the polls index\n{0}".format(template.render(context, request)))
+	#return HttpResponse("Hello World. You're at the polls index\n{0}".format(template.render(context, request)))
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question {0}".format(question_id))
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	#return HttpResponse("You're looking at question {0}".format(question_id))
+	return render(requests, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question {0}".format(question_id)
