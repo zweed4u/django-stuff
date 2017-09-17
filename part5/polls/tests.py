b6e79138eb5148a3bdcb51a3a321f24a3a38e148
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-09-17 19:48:00 -0400

    Fixed typo in variable assignment

--- part5/polls/tests.py
+++ part5/polls/tests.py
@@ -10,6 +10,6 @@ class QuestionModelTests(TestCase):
 		was_published_recently() should return False for questions whose pub_date
 		is in the future
 		"""
-		time = timezone.now() = datetime.timedelta(days=30)
+		time = timezone.now() + datetime.timedelta(days=30)
 		future_question = Question(pub_date=time)
 		self.assertIs(future_question.was_published_recently(), False)
