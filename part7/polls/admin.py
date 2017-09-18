1650f211feb25735d4f130486db1e717f2a23822
Zachary Weeden <zweeden@datto.com>
2017-09-18 13:08:05 -0400

    Changed the admin view header for was recently published and made use of boolean icon as well as filter for publish date - check method in model - also added search field for question text

--- part7/polls/admin.py
+++ part7/polls/admin.py
@@ -20,6 +20,8 @@ class QuestionAdmin(admin.ModelAdmin):
 	]
 	inlines = [ChoiceInline]
 	list_display = ('question_text','pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
--- part7/polls/models.py
+++ part7/polls/models.py
@@ -9,13 +9,16 @@ class Question(models.Model):
 	"""
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
-	
+
 	def was_published_recently(self):
 		now = timezone.now()
 		#Would return true for future questions
 		#return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 		#Fix by constraining pub_date on both ends
-		return now - datetime.timedelta(days=1) <= self.pub_date <= now		
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 	def __str__(self):
 		return self.question_text
