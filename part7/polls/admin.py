d0d9bdd3675eef1b19d3b658a0a46269440266af
Zachary Weeden <zweeden@datto.com>
2017-09-18 13:00:03 -0400

    Changed view of the admin question change page to show the question text, date published and the was published recently method for each entry

--- part7/polls/admin.py
+++ part7/polls/admin.py
@@ -19,6 +19,7 @@ class QuestionAdmin(admin.ModelAdmin):
 		})
 	]
 	inlines = [ChoiceInline]
+	list_display = ('question_text','pub_date', 'was_published_recently')
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
