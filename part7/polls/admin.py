d0e142d49a0a0bf69ce2d5babbdb9e7af022b33c
Zachary Weeden <zweeden@datto.com>
2017-09-18 12:46:37 -0400

    Modified the admin page to split up form into fields - date information section for date published field

--- part7/polls/admin.py
+++ part7/polls/admin.py
@@ -4,7 +4,12 @@ from .models import Question
 class QuestionAdmin(admin.ModelAdmin):
 	# Order of fileds as seen on http://127.0.0.1:8000/admin/polls/question/1/change/
 	# Date published then question text, reorder elements to see difference in position
-	fields = ['pub_date', 'question_text']
+	#fields = ['pub_date', 'question_text']
+	# Now splitting the form into fieldsets
+	fieldsets = [
+		(None, {'fields':['question_text']}),
+		('Date information', {'fields':['pub_date']})
+	]
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
