f8c06753e98e2c6dd9c0dd65b499e71e9fe4a481
Zachary Weeden <zweeden@datto.com>
2017-09-18 12:43:22 -0400

    Testing admin form UI tweaks - reordering fields using class and site register method

--- part7/polls/admin.py
+++ part7/polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Question
 
+class QuestionAdmin(admin.ModelAdmin):
+	# Order of fileds as seen on http://127.0.0.1:8000/admin/polls/question/1/change/
+	# Date published then question text, reorder elements to see difference in position
+	fields = ['pub_date', 'question_text']
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
